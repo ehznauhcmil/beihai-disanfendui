72a77d8587559e12bf54b7c892358457b74a8fc2
Lim Chuan Zhe <limcz0208@gmail.com>
2025-11-26 02:44:19 +0800

    History partially done

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+
+const historyCollection = defineCollection({
+  type: 'data',
+  schema: z.array(z.object({
+    year: z.string(),
+    title: z.string(),
+    description: z.string(),
+    era: z.string(),
+    image: z.string().optional(),
+    images: z.array(z.string()).optional(),
+    imageLayout: z.enum(['row', 'col']).optional().default('col'),
+    imageAlign: z.string().optional().default('center'),
+  })),
+});
+
+export const collections = {
+  'history': historyCollection,
+};
